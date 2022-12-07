122671d18ae24ffd7b7277d005f0ab9b0a4cfe1f
Ishan Dandekar <ishandandekar2002@gmail.com>
2022-12-07 16:05:01 +0530

    Update utility script

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -2,10 +2,12 @@
 This python script contains utility or helper functions which might help in the project
 """
 
+import os
 import zipfile
-from typing import Optional, Dict
+from typing import Dict
 
-def unzip_data(filename: str,data_dir: Optional[str]="data") -> None:
+
+def unzip_data(filename: str, data_dir: str ="data") -> None:
     """Uzips the files and folders into a directory
 
     Parameters
@@ -15,16 +17,23 @@ def unzip_data(filename: str,data_dir: Optional[str]="data") -> None:
     data_dir : str, optional
         Directory you want the zip folder you want the files extracted into, by default "data"
     """
+    if not os.path.exists(data_dir):
+        os.mkdir(data_dir)
     zip_ref = zipfile.ZipFile(filename, "r")
     zip_ref.extractall(data_dir)
     zip_ref.close()
 
-from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix
-import numpy as np
-from typing import Optional, Dict
+from typing import Dict
+
 import matplotlib.pyplot as plt
+import numpy as np
+import numpy.typing as npt
 import seaborn as sns
-def get_metrics(y_true: np.array, y_pred: np.array, plot_conf_matrix: Optional[bool]=False) -> Dict[str, float]:
+from sklearn.metrics import (accuracy_score, confusion_matrix, precision_score,
+                             recall_score)
+
+
+def get_metrics(y_true: npt.ArrayLike, y_pred: npt.ArrayLike, plot_conf_matrix: bool = False) -> Dict[str, float]:
     """Returns classification metrics (includes accuracy, precision, recall) and optionally plots a confusion matrix
 
     Parameters
@@ -33,7 +42,7 @@ def get_metrics(y_true: np.array, y_pred: np.array, plot_conf_matrix: Optional[b
         True labels
     y_pred : np.array
         Predicted labels
-    plot_conf_matrix : Optional[bool], optional
+    plot_conf_matrix : bool, optional
         Plots confusion matrix, by default False
 
     Returns
@@ -41,7 +50,7 @@ def get_metrics(y_true: np.array, y_pred: np.array, plot_conf_matrix: Optional[b
     Dict
         Contains metrics where key is the metric type
     """
-    y_true, y_pred = y_true.reshape(-1, 1), y_pred.reshape(-1, 1)
+    y_true, y_pred = np.array(y_true).reshape(-1, 1), np.array(y_pred).reshape(-1, 1)
     metrics_dict = {'accuracy': float(accuracy_score(y_true, y_pred)), 'precision': float(precision_score(y_true, y_pred)), 'recall': float(recall_score(y_true, y_pred))}
     if plot_conf_matrix:
         cm = confusion_matrix(y_true, y_pred, normalize=True)
